845661d89159675c7761d0ff250a950ada238458
Nicolas Debard <ndebard@ec-m.fr>
2014-11-06 08:30:47 +0100

    test 2

---
+++ src/main/java/domain/Task.java
@@ -0,0 +1,58 @@
+package main.java.domain;
+
+/**
+ * Created by nicolasdebard on 05/11/2014.
+ */
+import java.util.Date;
+
+public class Task {
+    private Date createdAt = new Date();
+
+    private String title;
+
+    private String text;
+
+    private Date date;
+
+    private String tags;
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
